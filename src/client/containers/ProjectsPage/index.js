73d73dc912ab99b5aaab87d6559545829d41f39e
Casey Van Groll <caseyvangroll@gmail.com>
2018-11-05 19:28:53 -0600

    fix casing of ProjectsPage index

--- src/client/containers/ProjectsPage/index.js
+++ src/client/containers/ProjectsPage/index.js
@@ -1,47 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { createProject } from "../../redux/actions/projectActions";
-// import { Header, Sidebar } from '../../components';
-import { Project } from "../../components";
-import { Container, Card, Button } from "reactstrap";
 
 class ProjectsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { projects } = this.props;
-    const projectsList = projects.map(project => (
-      <Project key={project.id} {...project} />
-    ));
-
-    return (
-      <Container fluid={true} id="PROJECTS_PAGE">
-        {/* <Header />
-                <Sidebar /> */}
-        <main>
-          {projectsList}
-          <Card className="project">
-            <Button>New</Button>
-          </Card>
-        </main>
-      </Container>
-    );
-  }
+  render() {}
 }
 
-const mapStateToProps = state => ({
-  user: state.user,
-  projects: state.projects
-});
-
-const mapDispatchToProps = {
-  createProject
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ProjectsPage);
+export default connect()(ProjectsPage);
