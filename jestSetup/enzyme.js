1f5b62f8cc8e18b892d44422fefb0ad7c513ea81
Casey Van Groll <caseyvangroll@gmail.com>
2018-11-12 12:53:42 -0600

    Revert "Revert "Merge branch 'project/component' into 'develop'""
    This reverts commit 0d03b74194e12a6d383d3d025feeefc5025dd6db.

--- jestSetup/enzyme.js
+++ jestSetup/enzyme.js
@@ -1,5 +1,10 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// Add icons to library
+import { faEdit, faCheck, faBan, faTrashAlt, faHome, faUser, faUserCog, faFileAlt, faGraduationCap, faBriefcase, faCalendarAlt, faProjectDiagram, faAddressBook } from '@fortawesome/free-solid-svg-icons';
+library.add(faEdit, faCheck, faBan, faTrashAlt, faHome, faUser, faUserCog, faFileAlt, faGraduationCap, faBriefcase, faCalendarAlt, faProjectDiagram, faAddressBook);
 
 global.requestAnimationFrame = (cb) => {
     setTimeout(cb, 0);
--- src/client/routes.js
+++ src/client/routes.js
@@ -1,31 +1,38 @@
 import React from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
 import {
-  HomeScreen,
-  ContactsPage,
-  DocumentsPage,
-  EducationPage,
-  ExperiencePage,
-  ProfilePage,
-  ProjectsPage,
-  TimelinePage,
-  LoginPage
-} from "./containers";
+    HomeScreen,
+    ContactsPage,
+    DocumentsPage,
+    EducationPage,
+    ExperiencePage,
+    ProfilePage,
+    ProjectsPage,
+    TimelinePage,
+    LoginPage,
+} from './containers';
+import { Sidebar, Footer } from './components';
 
 const routes = () => (
-  <React.Fragment>
-    <Switch>
-      <Route exact path="/" component={HomeScreen} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/profile" component={ProfilePage} />
-      <Route path="/contacts" component={ContactsPage} />
-      <Route path="/documents" component={DocumentsPage} />
-      <Route path="/education" component={EducationPage} />
-      <Route path="/experience" component={ExperiencePage} />
-      <Route path="/projects" component={ProjectsPage} />
-      <Route path="/timeline" component={TimelinePage} />
-    </Switch>
-  </React.Fragment>
+    <React.Fragment>
+        <Switch>
+            <Route exact path="/" component={LoginPage} />
+            <Route exact path="/home" component={HomeScreen} />
+            <Route>
+                <React.Fragment>
+                    <Sidebar />
+                    <Route exact path="/profile" component={ProfilePage} />
+                    <Route exact path="/contacts" component={ContactsPage} />
+                    <Route exact path="/documents" component={DocumentsPage} />
+                    <Route exact path="/education" component={EducationPage} />
+                    <Route exact path="/experience" component={ExperiencePage} />
+                    <Route exact path="/projects" component={ProjectsPage} />
+                    <Route exact path="/timeline" component={TimelinePage} />
+                    <Footer />
+                </React.Fragment>
+            </Route>
+        </Switch>
+    </React.Fragment>
 );
 
 export default withRouter(routes);
