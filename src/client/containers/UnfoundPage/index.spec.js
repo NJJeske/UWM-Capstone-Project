e22cd52b4929492de893d0dfa377a371dc3cb8e3
Matthew Flejter <matthewflejter@gmail.com>
2018-11-13 00:15:11 -0600

    Adding unfound page container and sass file for it.

---
+++ src/client/containers/UnfoundPage/index.spec.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { UnfoundPage } from ".";
+import { shallow } from "enzyme";
+
+var wrapper = shallow(<UnfoundPage />);
+
+it("should render an outer div", () => {
+  expect(wrapper.exists("div"));
+});
+
+it("should render a first span that contains a fontawesome icon", () => {
+  expect(
+    wrapper
+      .find("span")
+      .at(0)
+      .text()
+  ).toEqual("<FontAwesomeIcon />");
+});
+
+it("should render a second span that says: 404 Not Found.", () => {
+  expect(
+    wrapper
+      .find("span")
+      .at(1)
+      .text()
+  ).toEqual(" 404 Not Found.");
+});
+
+it("should render a third span that says: The requested URL was not found on this server.", () => {
+  expect(
+    wrapper
+      .find("span")
+      .at(2)
+      .text()
+  ).toEqual("The requested URL was not found on this server.");
+});
