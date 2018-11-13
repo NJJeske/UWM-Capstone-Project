3b77c6fd50f53e44d26236dca9a23ae13b30baf3
Matthew Flejter <matthewflejter@gmail.com>
2018-11-13 00:13:55 -0600

    Created 404 not found page, the tests for it, and implemented it into routes.js.

--- src/client/app.js
+++ src/client/app.js
@@ -1,51 +1,53 @@
-'use strict';
-import '@babel/polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import store from './redux/store.js';
-import Routes from './routes.js';
-import 'bootstrap/dist/css/bootstrap.css';
-import { library } from '@fortawesome/fontawesome-svg-core';
+"use strict";
+import "@babel/polyfill";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store.js";
+import Routes from "./routes.js";
+import "bootstrap/dist/css/bootstrap.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import {
-    faEdit,
-    faCheck,
-    faBan,
-    faTrashAlt,
-    faHome,
-    faUser,
-    faUserCog,
-    faFileAlt,
-    faGraduationCap,
-    faBriefcase,
-    faCalendarAlt,
-    faProjectDiagram,
-    faAddressBook,
-    faSpinner
-} from '@fortawesome/free-solid-svg-icons';
+  faEdit,
+  faCheck,
+  faBan,
+  faTrashAlt,
+  faHome,
+  faUser,
+  faUserCog,
+  faFileAlt,
+  faGraduationCap,
+  faBriefcase,
+  faCalendarAlt,
+  faProjectDiagram,
+  faAddressBook,
+  faSpinner,
+  faExclamationCircle
+} from "@fortawesome/free-solid-svg-icons";
 library.add(
-    faEdit,
-    faCheck,
-    faBan,
-    faTrashAlt,
-    faHome,
-    faUser,
-    faUserCog,
-    faFileAlt,
-    faGraduationCap,
-    faBriefcase,
-    faCalendarAlt,
-    faProjectDiagram,
-    faAddressBook,
-    faSpinner
+  faEdit,
+  faCheck,
+  faBan,
+  faTrashAlt,
+  faHome,
+  faUser,
+  faUserCog,
+  faFileAlt,
+  faGraduationCap,
+  faBriefcase,
+  faCalendarAlt,
+  faProjectDiagram,
+  faAddressBook,
+  faSpinner,
+  faExclamationCircle
 );
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Routes />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('origin')
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("origin")
 );
--- src/client/containers/index.js
+++ src/client/containers/index.js
@@ -8,3 +8,4 @@ export { default as TimelinePage } from "./TimelinePage";
 export { default as LoginPage } from "./LoginPage";
 export { default as CallbackPage } from "./CallbackPage";
 export { default as ProjectsPage } from "./ProjectsPage";
+export { default as UnfoundPage } from "./UnfoundPage";
