f37ae61869bdb2cd1e5f907a73b3c57bc95f57f1
Casey Van Groll <caseyvangroll@gmail.com>
2018-11-17 19:21:52 -0600

    adjust address in mock

--- src/server/mock/generator/address.js
+++ src/server/mock/generator/address.js
@@ -1,10 +1,9 @@
 module.exports = (faker, rand) => {
     return {
-        id: faker.random.uuid(),
         street1: faker.address.streetAddress(),
         street2: rand.maybe(faker.address.secondaryAddress(), 0.2),
         city: faker.address.city(),
-        state: faker.address.state(),
-        zipCode: faker.address.zipCode()
+        state: faker.address.stateAbbr(),
+        zip: faker.address.zipCode('#####')
     };
 };
