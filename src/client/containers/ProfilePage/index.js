fa8d9ece81d2868dd641cc49e515d4f95667078d
Matthew Flejter <matthewflejter@gmail.com>
2018-11-25 16:32:59 -0600

    Adjusted styling.

--- src/client/containers/ProfilePage/index.js
+++ src/client/containers/ProfilePage/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ProfileForm, Sidebar } from '../../components';
+import { ProfileForm, Sidebar, Header } from '../../components';
+import '../../sass/_profilepage.scss';
 
 class ProfilePage extends Component {
     render() {
         return (
-            <div>
+            <div className='profileBody'>
+                <Header title={'Profile Settings'} />
                 <Sidebar />
                 <ProfileForm />
             </div>
